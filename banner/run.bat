e9593bff6cc2ede236bae89f17ad7f29b790aff4
Chandrasekaran Janardhanan <jjchandru@yahoo.com>
2023-09-07 16:59:24 +0530

    modified run.bat to remove echo on / off

--- banner/run.bat
+++ banner/run.bat
@@ -1,5 +1,3 @@
-@echo off
 curl -d "" -H "content-type:application/json" http://localhost:8081/actuator/shutdown
 start /B java -jar -Dserver.port=8081 target/banner-1.0.jar > dev.log
-@echo on
 exit 0
